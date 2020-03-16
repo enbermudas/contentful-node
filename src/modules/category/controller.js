3122882a6cd284e92d99310b098e13502a52bc59
Enrique Bermúdez <enbermudez96@gmail.com>
2020-03-16 15:37:04 -0300

    feat: create category module

---
+++ src/modules/category/controller.js
@@ -0,0 +1,16 @@
+import request from 'utils/request';
+import logger from 'config/logger';
+
+const get = async (req, res, next) => {
+  try {
+    const response = await request.get('content_types/category');
+    return res.json(response.data);
+  } catch (error) {
+    logger.error(error.response.data.message);
+    return next(error);
+  }
+};
+
+module.exports = {
+  get,
+};
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import categoryRoutes from 'modules/category/routes';
 
 const router = express.Router();
 
 router.get('/status', (req, res) => {
   res.send({ status: 'online' });
 });
+
+router.use('/category', categoryRoutes);
 
 export default router;
