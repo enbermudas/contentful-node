6fd167197ac7a017780ca75bb20b58b4905369b9
Enrique Bermúdez <enbermudez96@gmail.com>
2020-03-16 16:39:55 -0300

    test: add server test

---
+++ tests/server.test.js
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import app from '../src/config/express';
+
+const server = app.listen(3000);
+
+afterAll(() => server.close());
+
+describe('Server', () => {
+  test('it should start the server', async () => {
+    const res = await request(server).get('/api/status');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('status');
+  });
+});
