0ef0561c1a0eef5b28ca79b3cbdb2d3b99063b6c
Enrique Bermúdez <enbermudez96@gmail.com>
2020-03-17 10:05:48 -0300

    chore: add env variables to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
+process.env.NODE_ENV = 'test';
+process.env.HOST = '0.0.0.0';
+process.env.PORT = 3030;
+
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js', '!data/keyMap.js', '!/node_modules/'],
   clearMocks: true,
   testEnvironment: 'node',
   coveragePathIgnorePatterns: ['/node_modules/', '/coverage/'],
+  setupFilesAfterEnv: ['./tests/setup.js'],
 };
